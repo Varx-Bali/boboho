1b1d087c594f947314bd7a74f98fa89b1e44232e
lexent007 <vwilly007@gmail.com>
2021-10-13 18:06:27 +0800

    Update index.js
    -update title welcome

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World! Welcome to the jungle!!!
+          Hello World! Welcome to Red White
         </h1>
       </main>
 
