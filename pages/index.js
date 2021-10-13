877d37a851c7d7c8bdcbca390ddb8db853342c34
lexent007 <vwilly007@gmail.com>
2021-10-13 18:09:05 +0800

    Update index.js
    -update title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World! Welcome to Red White Coffee
+          Hello World! Welcome to Red White Coffee!
         </h1>
       </main>
 
