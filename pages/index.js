1d3d3f937c0aeac508fc0d6ca0f011fb6345c4a2
lexent007 <vwilly007@gmail.com>
2021-10-13 17:05:16 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World! Welcome to the jungle!!!
+          Hello World! Welcome to the jungle mother father!
         </h1>
       </main>
 
